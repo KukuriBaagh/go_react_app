aa7e4af30185520c18b82fa55cab0a84bf9206d1
KukuriBaagh <howtoccdemo@gmail.com>
2024-03-11 11:04:33 +0530

    finished product

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,14 @@
-import { Box } from '@mantine/core';
+import { Box, List, ThemeIcon } from '@mantine/core';
 import useSWR from 'swr';
 import AddTodos from './components/AddTodos';
+import { CheckCircleFillIcon } from '@primer/octicons-react';
+
+export interface Todo {
+  id: number;
+  title: string;
+  body: string;
+  done: boolean;
+}
 
 export const ENDPOINT = 'http://localhost:4000';
 
@@ -8,12 +16,48 @@ const fetcher = (url: string) =>
   fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
 
 const App = () => {
-  const { data, mutate } = useSWR('api/todos', fetcher);
+  const { data, mutate } = useSWR<Todo[]>('api/todos', fetcher);
+
+  async function markTodoAdDone(id: number) {
+    const updated = await fetch(`${ENDPOINT}/api/todos/${id}/done`, {
+      method: 'PATCH',
+    }).then((r) => r.json());
+    mutate(updated);
+  }
 
   return (
-    <Box>
-      {JSON.stringify(data)}
-      <AddTodos />
+    <Box
+      style={(theme) => ({
+        padding: '2rem',
+        width: '100%',
+        maxWidth: '40rem',
+        margin: '0 auto',
+      })}
+    >
+      <List spacing='xs' size='sm' mb={12} center>
+        {data?.map((todo) => {
+          return (
+            <List.Item
+              onClick={() => markTodoAdDone(todo.id)}
+              key={`todo_list__${todo.id}`}
+              icon={
+                todo.done ? (
+                  <ThemeIcon color='teal' size={24} radius='xl'>
+                    <CheckCircleFillIcon size={20} />
+                  </ThemeIcon>
+                ) : (
+                  <ThemeIcon color='gray' size={24} radius='xl'>
+                    <CheckCircleFillIcon size={20} />
+                  </ThemeIcon>
+                )
+              }
+            >
+              {todo.title}
+            </List.Item>
+          );
+        })}
+      </List>
+      <AddTodos mutate={mutate} />
     </Box>
   );
 };
